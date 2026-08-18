21f382a789631169e0696f24e5b0f90e7cff4d68
InsanePogU <szczupakapike@gmail.com>
2026-08-18 12:21:14 +0200

    fixing data to learn

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from scripts.DataLoader import DataLoader
 
 
@@ -5,8 +6,11 @@ from scripts.DataLoader import DataLoader
 def main():
     df=DataLoader("data/WA_Fn-UseC_-Telco-Customer-Churn.csv") #tworzymy sobie obiekt
     data=df.load()
+
+    with open("data/file.csv","w") as f:
+        pd.DataFrame(data).to_csv(f,index=False)
+
     print(data.head())
-    print(data.info())
 
 
 if __name__ == "__main__":
--- scripts/DataLoader.py
+++ scripts/DataLoader.py
@@ -6,25 +6,25 @@ class DataLoader:
     def __init__(self, path):
         self.path=path
 
-
     def load(self):
-        return pd.read_csv(self.path)
+        df_tmp = pd.read_csv(self.path)
+        df_tmp['TotalCharges'] = pd.to_numeric(df_tmp['TotalCharges'], errors='coerce')
+        df_tmp = df_tmp.dropna()
+        df_tmp = df_tmp.drop(columns=['customerID'])
+        df = pd.get_dummies(df_tmp, drop_first=True, dtype=int)
+        return df
 
-
-    def split_data_x_y(self):
+    def split_data_x_y(self): #split data and change for 0 and 1
         df=self.load()
-        df['gender'] = df['gender'].replace({'Male': 1, 'Female': 0})
-        df['partner']=df['partner'].replace({'Yes': 1, 'No': 0})
-        df['OnlineSecurity'] = df['OnlineSecurity'].replace({'Yes': 1, 'No': 0, 'No internet service': 0})
-        
-        df[]
-        X=df.drop(columns=['customerID', 'Churn']) #
-        Y=df['Churn']
-        return X,Y
+        x=df.drop(columns=['Churn_Yes'])
+        y=df['Churn_Yes']
+        return x,y
 
 
-    def set_train_test_split(self,X,Y): #testowe i treningowe zbiory
-        pass
+    def set_train_test_split(self): #testowe i treningowe zbiory
+        x,y=self.split_data_x_y()
+        x_train,x_test,y_train,y_test=sk.train_test_split(x,y,test_size=0.2)
+        return x_train,y_train,x_test,y_test
 
 
 
