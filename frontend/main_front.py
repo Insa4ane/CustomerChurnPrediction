57c9607188b5dafe8d758546c3783860e4457728
InsanePogU <szczupakapike@gmail.com>
2026-09-23 11:48:49 +0200

    finish the app

--- frontend/main_front.py
+++ frontend/main_front.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from config.config import COLUMNS_TYPES
-from frontend.frontend import Frontend
+from ui import Frontend
 import joblib
 
 def main_menu():
@@ -9,8 +9,15 @@ def main_menu():
     frontend = Frontend(columns_with_types)
     create_form, written_data=frontend.main_menu()
     if create_form and written_data:
-        response=frontend.send_data(create_form, written_data)
-        st.success(response)
+        response = frontend.send_data(create_form, written_data)
+        if response is not None and response.status_code == 200:
+            result = response.json()["result"]
+            if result == "Yes":
+                st.success("Klient najprawdopodobniej zrezygnuje z usługi.")
+            else:
+                st.success("Klient najprawdopodobniej pozostanie z nami.")
+        else:
+            st.error(f"Błąd zapytania: {response}")
 
 
 if __name__ == "__main__":
